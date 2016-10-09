36e8a109ede660797208f02964ca0e14d37e93b8
Hugo Jugo <Hugo Jugo>
2016-10-09 18:47:14 -0500

    archivos nuevos usando atom y git al mismo tiempo

---
+++ aprendePython.py
@@ -0,0 +1,21 @@
+# este programa es para aprender python
+
+monty = 23  # el perro es el que no hace nada
+perro = 24
+
+var = ("la wea fome de tu madre" + "es la que molesta mucho")
+
+print(len(var))
+
+perro2 = monty + perro + 1
+
+print (perro2)
+
+d = {'clave1':23,'clave2':34}
+
+print (d)
+
+
+
+
+#
